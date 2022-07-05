2d2ad322aa662f3e332dc46dec35aae0179d572d
atsushi0919 <hatake0919@yahoo.co.jp>
2022-07-05 16:23:02 +0900

    5-4 admin, customer の設定追加

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,4 @@
 Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.paths << Rails.root.join("node_modules")
 # アセットプリコンパイルの対象に追加
-Rails.application.config.assets.precompile += %w(staff.css)
+Rails.application.config.assets.precompile += %w(staff.css admin.css customer.css)
